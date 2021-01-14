554375b75f3c53720eb763e8e094273c3d5c9569
E5cu3l4DI* <escueladigitalcarvajal@gmail.com>
2021-01-13 22:01:55 -0500

    dockerfile

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,8 @@
 export default {
   // Global page headers (https://go.nuxtjs.dev/config-head)
-  ssr: false,
+  server: {
+    host: '0.0.0.0'
+  },
   head: {
     title: 'torrePrueba',
     meta: [
